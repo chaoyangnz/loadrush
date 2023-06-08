10de452fdfba1f929b8a45ad758935433bb5c099
Chao Yang <chao.yang@siteminder.com>
2023-06-08 21:08:22 +1200

    fix config resovling

--- src/loadrush/actions/think.ts
+++ src/loadrush/actions/think.ts
@@ -5,7 +5,7 @@ import { ActionContext } from '../context';
 export function think(ms: number): Action {
   return {
     type: ActionType.STEP,
-    title: repeat('.', ms / 1000),
+    title: repeat('.', ms / 1000) || '🤔',
     run: async (context: ActionContext) => {
       return new Promise<void>((resolve, reject) => {
         setTimeout(resolve, ms);
